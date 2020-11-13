a95793f2123c6c3673d9c2617fcba3ca24f96b01
sandeep-srinivas-my-personal <74413035+sandeep-srinivas-my-personal@users.noreply.github.com>
2020-11-13 18:37:52 +0530

    Create Day_1.py

---
+++ Day_1.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city = input("What's name of the city you grew up in?\n")
+pet = input("what's your pet name?\n")
+print("your band name could be "+ city +" "+pet)
